10e78391e8b76575d3348f6d920e8b2314cb377b
Shane Padellaro <shane.padellaro@airfinetworks.com>
2019-04-30 21:42:07 +0100

    turned into package

--- main.go
+++ main.go
@@ -2,55 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
-
-	"go-test/decisionTree"
+	"go-test/treeEnsemble"
 
 	"github.com/kniren/gota/dataframe"
 	"github.com/kniren/gota/series"
 )
-
-type TreeEnsemble struct {
-	x, y                             dataframe.DataFrame
-	numberTress, sampleSize, minLeaf int
-	trees                            []*decisionTree.DecisionTree
-}
-
-func New(x dataframe.DataFrame, y dataframe.DataFrame, numberTress int, sampleSize int, minLeaf int) *TreeEnsemble {
-	ensemble := new(TreeEnsemble)
-	ensemble.y = y
-	ensemble.x = x
-	ensemble.numberTress = numberTress
-	ensemble.sampleSize = sampleSize
-	ensemble.minLeaf = minLeaf
-	ensemble.trees = make([]*decisionTree.DecisionTree, 0)
-
-	for i := 0; i < numberTress; i++ {
-		ensemble.trees = append(ensemble.trees, ensemble.CreateTree())
-	}
-
-	return ensemble
-}
-
-func (ensemble *TreeEnsemble) CreateTree() *decisionTree.DecisionTree {
-	indexes := make([]int, 0)
-	rows := ensemble.y.Nrow()
-	for j := 0; j < rows; j++ {
-		indexes = append(indexes, j)
-	}
-	shuffle(indexes)
-	rndIndexes := indexes[:ensemble.sampleSize]
-
-	tree := decisionTree.New(ensemble.x.Subset(rndIndexes), ensemble.y.Subset(rndIndexes), ensemble.minLeaf)
-	return &tree
-}
-
-func shuffle(indexes []int) {
-	d := len(indexes)
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(d, func(i, j int) { indexes[i], indexes[j] = indexes[j], indexes[i] })
-}
 
 func main() {
 
@@ -60,7 +16,7 @@ func main() {
 		series.New([]float64{3.0, 4.0, 5.0, 6.0}, series.Float, "COL.3"),
 	)
 
-	randomForest := New(df, df, 5, 2, 2)
+	randomForest := treeEnsemble.New(df, df, 5, 2, 2)
 
 	fmt.Println(df)
 	fmt.Println(randomForest)
---
+++ treeEnsemble/treeEnsemble.go
@@ -0,0 +1,51 @@
+package treeEnsemble
+
+import (
+	"math/rand"
+	"time"
+
+	"go-test/decisionTree"
+
+	"github.com/kniren/gota/dataframe"
+)
+
+type TreeEnsemble struct {
+	x, y                             dataframe.DataFrame
+	numberTress, sampleSize, minLeaf int
+	trees                            []*decisionTree.DecisionTree
+}
+
+func New(x dataframe.DataFrame, y dataframe.DataFrame, numberTress int, sampleSize int, minLeaf int) *TreeEnsemble {
+	ensemble := new(TreeEnsemble)
+	ensemble.y = y
+	ensemble.x = x
+	ensemble.numberTress = numberTress
+	ensemble.sampleSize = sampleSize
+	ensemble.minLeaf = minLeaf
+	ensemble.trees = make([]*decisionTree.DecisionTree, 0)
+
+	for i := 0; i < numberTress; i++ {
+		ensemble.trees = append(ensemble.trees, ensemble.CreateTree())
+	}
+
+	return ensemble
+}
+
+func (ensemble *TreeEnsemble) CreateTree() *decisionTree.DecisionTree {
+	indexes := make([]int, 0)
+	rows := ensemble.y.Nrow()
+	for j := 0; j < rows; j++ {
+		indexes = append(indexes, j)
+	}
+	shuffle(indexes)
+	rndIndexes := indexes[:ensemble.sampleSize]
+
+	tree := decisionTree.New(ensemble.x.Subset(rndIndexes), ensemble.y.Subset(rndIndexes), ensemble.minLeaf)
+	return &tree
+}
+
+func shuffle(indexes []int) {
+	d := len(indexes)
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(d, func(i, j int) { indexes[i], indexes[j] = indexes[j], indexes[i] })
+}
